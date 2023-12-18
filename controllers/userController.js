1c6a9aa151963631b8db6e6fbabec2e5a52a864f
Prithu Singh <prithusingh0327@gmail.com>
2023-12-18 20:36:12 +0530

    Finished userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,16 +12,65 @@ const UserController = {
 
             const { name, email, age, selectedSlot } = req.body;
 
-            // Check if the user already exists
-            const existingUser = await User.findOne({ where: { email } });
-            if (existingUser) {
-                return res
-                    .status(400)
-                    .json({ error: "User with this email already exists." });
+            // Check if the user already enrolled in the current month
+            const currentMonth = new Date().toISOString().slice(0, 7); // Format: 'YYYY-MM'
+            const existingUserInCurrentMonth = await Transaction.findOne({
+                where: {
+                    userID: email,
+                    enrollmentMonth: currentMonth,
+                },
+            });
+
+            if (existingUserInCurrentMonth) {
+                return res.status(400).json({
+                    error: "User already enrolled in this month. Switch next month.",
+                });
             }
 
             // Create a new user
             const newUser = await User.create({ name, email, age });
+
+            // Fetch the selected slot information
+            const selectedSlotInfo = await Slot.findByPk(selectedSlot);
+            if (!selectedSlotInfo) {
+                return res
+                    .status(404)
+                    .json({ error: "Selected slot not found." });
+            }
+
+            // Create a transaction for the user
+            const newTransaction = await Transaction.create({
+                amount: selectedSlotInfo.price,
+                userID: newUser.email,
+                slotID: selectedSlot,
+                enrollmentMonth: currentMonth,
+            });
+
+            // Mocked CompletePayment function
+            const CompletePayment = async ({ user, transaction }) => {
+                return { success: true, message: "Payment successful." };
+            };
+
+            const paymentResponse = await CompletePayment({
+                user: newUser,
+                transaction: newTransaction,
+            });
+
+            // Handle the payment response
+            if (paymentResponse.success) {
+                return res.status(200).json({
+                    success: true,
+                    message: "Registration successful!",
+                });
+            } else {
+                // If payment fails, rollback the transaction and delete the user
+                await newTransaction.destroy();
+                await newUser.destroy();
+
+                return res
+                    .status(500)
+                    .json({ error: "Payment failed. Please try again." });
+            }
         } catch (error) {
             console.error(error);
             return res.status(500).json({ error: "Internal server error." });
